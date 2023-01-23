4bdb34dc83459eda431ae479f477ccb8dcfb0037
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-01-23 09:41:10 -0300

    ✨ feat: add form to enter in the game

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import styles from './app.module.scss';
-
 import web3 from './provider/web3';
 import lottery from './provider/lottery';
 
@@ -15,7 +14,10 @@ type Info = {
 
 const App: React.FC = () => {
 
+	const inputAmountRef = useRef<HTMLInputElement>(null);
+	const inputNameRef = useRef<HTMLInputElement>(null);
 	const [info, setInfo] = useState<Info>();
+	const [status, setStatus] = useState<string>('');
 
   useEffect(() => {
     getInfo();
@@ -35,13 +37,47 @@ const App: React.FC = () => {
 		});
 	}
 
+	async function onSubmit(event: React.FormEvent) {
+		event.preventDefault();
+		setStatus('Waiting on transaction success...')
+		const name = inputNameRef.current?.value;
+		const amount = inputAmountRef.current?.value;
+		const accounts = await web3.eth.getAccounts();
+		await lottery.methods.enter(name).send({
+			from: accounts[0],
+			value: web3.utils.toWei(`${amount}`, 'ether'),
+		});
+		setStatus('You have been entered!');
+	}
+
   return (
     <main className={styles.container}>
 			<div>
 				<h2>Lottery Contract</h2>
 				<p>This contract is managed by {info?.manager}</p>
-				<p>There are currently {info?.totalPlayers} people entered, competing to win {web3.utils.fromWei(info?.balance || '0', 'ether')} ether!</p>
+				<p>
+					There are currently {info?.totalPlayers} people entered, competing to win 
+					{` ${web3.utils.fromWei(info?.balance || '0', 'ether')}`} ether!
+				</p>
 			</div>
+
+			<hr />
+
+			<form className={styles.form} onSubmit={onSubmit}>
+				<h4>Want to try your luck?</h4>
+				<div>
+					<label>Your name:</label>
+					<input ref={inputNameRef} />
+					<label>Amount of ether to enter:</label>
+					<input ref={inputAmountRef} type='number' min='0.11' />
+					<button type='submit'>Enter</button>
+				</div>
+			</form>
+			{status && (
+				<div className={styles.load}>
+					<span>{status}</span>
+				</div>
+			)}
 		</main>
   );
 }
